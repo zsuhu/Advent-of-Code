31d0fd8c3760039c3641d92101703b157a278e62
Zakki Suhu <suhu.zakki@gmail.com>
2025-12-03 23:08:23 -0500

    Create day1_part2.py

---
+++ 2025/day1_part2.py
@@ -0,0 +1,31 @@
+actual = 50
+zeros = 0
+
+with open("day1_testcase.txt") as f:
+    for line in f:
+        direction = line[:1]
+        distance = int(line.strip()[1:len(line)])
+        
+        if direction == 'R':
+            if actual + distance > 99:
+                zeros += (actual + distance) // 100
+                actual = (actual + distance) % 100
+            else:
+                actual += distance 
+                
+        elif direction == 'L':
+            if actual == 0:
+                actual = (actual - distance) % 100
+            elif actual - distance < 0:
+                zeros += abs((actual - distance) // 100)
+                actual = (actual - distance) % 100
+            elif actual - distance == 0:
+                zeros += 1
+                actual -= distance
+            else:
+                actual -= distance
+                
+            
+        print(actual, zeros)
+                
+print("password:", zeros) #L55, 55->0
